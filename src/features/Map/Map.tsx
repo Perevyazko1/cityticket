2e5b8673d8902a2d0aabcd24d9ef52999e0dde42
Perevyazko <a.perevyazko@gmail.com>
2024-01-28 20:05:48 +0300

    setting window

--- src/features/Map/Map.tsx
+++ src/features/Map/Map.tsx
@@ -1,63 +1,83 @@
 import {attributesSvgType} from 'entity/Map';
-import {memo, ReactNode, useEffect, useRef, useState} from 'react';
+import {memo, ReactNode, ReactSVGElement, useEffect, useRef, useState} from 'react';
 import {classNames, Mods} from "shared/lib/classNames/classNames";
 import {ReactSVG} from 'react-svg';
 import {Button} from "shared/ui/Button/Button";
 import cls from "./Map.module.scss"
+import {Simulate} from "react-dom/test-utils";
+import click = Simulate.click;
 
 
 interface MapProps {
     className?: string
     children?: ReactNode
     apiSvg: string
+    scale: number
 }
 
 
 export const Map = memo((props: MapProps) => {
-    const [scale, setScale] = useState(0.41);
+    const [listId, setListId] = useState<string[]>(['parter_1_1', 'parter_2_1', 'parter_3_1', 'parter_4_1', 'parter_5_1', 'parter_8_1', 'parter_9_1', 'parter_10_1', 'parter_11_12', 'beltaj_right_4_8'])
     const svgRef = useRef(null)
 
-const handleElementClick = (event: any) => {
-  const clickedElement = event.target;
 
-  // Получить координаты и размеры элемента
-  const boundingBox = clickedElement.getBoundingClientRect();
-  const originalWidth = boundingBox.width;
-  const originalHeight = boundingBox.height;
-  const originalX = boundingBox.left;
-  const originalY = boundingBox.top;
+    const handleElementClick = (event: any) => {
+        const clickedElement = event.target;
+        // if (listId.includes(clickedElement.id)) {
+        //     setListId(prevListId => prevListId.filter(id => id !== clickedElement.id));
+        // } else {
+        //     setListId([...listId, clickedElement.id])
+        // }
 
-  const newWidth = originalWidth * scale;
-  const newHeight = originalHeight * scale;
+        console.log(listId)
+        // Применить цвет или другие стили, используя новые координаты и размеры
+        if (clickedElement.style.fill === "red") {
+            clickedElement.style.fill = "";
+        } else {
+            clickedElement.style.fill = "red";
+        }
 
-  // Пересчитать координаты клика относительно нового масштаба
-  const clickX = (event.clientX - originalX) * (scale - 1);
-  const clickY = (event.clientY - originalY) * (scale - 1);
-console.log(clickedElement.style.fill)
-  // Применить цвет или другие стили, используя новые координаты и размеры
-    if (clickedElement.style.fill === "red"){
-        clickedElement.style.fill = "";
-    }else {
-        clickedElement.style.fill = "red";
+        // Применить другие стили, используя новые координаты и размеры
+
+        // setSeatList(prevList => [...prevList, 'новое значение']);
+    };
+
+    const update = () => {
+        listId.map((itemId) => {
+            const click = document.getElementById(itemId)
+            if (click) {
+                click.classList.add('red')
+                click.setAttribute('style', 'fill: red'); // Установка цвета напрямую через атрибут fill
+                click.style.fill = "red"; // Обновление стиля для немедленного отображения изменений
+            }
+            // if (elementSvg) {
+            //     elementSvg.setAttribute('fill', 'red');
+            //     // console.log(elementSvg)
+            //     // Здесь вы можете выполнить нужные действия с найденным элементом SVG
+            // }
+        })
+    };
+
+
+    const handletest = () => {
+        console.log("функция сработала")
+        listId.map((itemId) => {
+            const click = document.getElementById(itemId)
+            if (click) {
+                click.classList.add('red')
+                click.setAttribute('style', 'fill: red'); // Установка цвета напрямую через атрибут fill
+                click.style.fill = "red"; // Обновление стиля для немедленного отображения изменений
+            }
+        });
     }
-
-  // Применить другие стили, используя новые координаты и размеры
-
-  // setSeatList(prevList => [...prevList, 'новое значение']);
-};        const handleZoomIn = () => {
-        setScale(scale => scale * 1.2);
-    };
-
-    const handleZoomOut = () => {
-        setScale(scale => scale * 0.8);
-    };
-
-
-
+    useEffect(() => {
+        handletest(); // Вызов функции handletest при монтировании компонента
+    }, []);
     const {
         className,
         children,
         apiSvg,
+        scale,
         ...otherProps
     } = props
 
@@ -65,7 +85,7 @@ console.log(clickedElement.style.fill)
 
     return (
         <div
-            className={classNames("", mods, [className])}
+            className={classNames(cls.Map, mods, [className])}
             {...otherProps}
         >
             <ReactSVG
@@ -73,11 +93,8 @@ console.log(clickedElement.style.fill)
                 src={apiSvg}
                 style={{transform: `scale(${scale})`, transformOrigin: 'left top'}}
                 onClick={handleElementClick}
+                ref={svgRef}
             />
-            <div className={cls.buttonGroup}>
-                <Button onClick={handleZoomIn}>+</Button>
-                <Button onClick={handleZoomOut}>-</Button>
-            </div>
 
         </div>
     );
--- src/features/MapLoader/MapLoader.tsx
+++ src/features/MapLoader/MapLoader.tsx
@@ -4,6 +4,9 @@ import axios from "axios";
 import {Map} from "../Map/Map";
 import {mapData} from "entity/MapLoader";
 import {MapDrag} from "../MapDrag/MapDrag";
+import svg from "./converted.svg"
+import {Button} from "../../shared/ui/Button/Button";
+import cls from "./Maploader.module.scss";
 
 
 interface MapLoaderProps {
@@ -13,28 +16,40 @@ interface MapLoaderProps {
 
 
 export const MapLoader = memo((props: MapLoaderProps) => {
+        const [scale, setScale] = useState(0.41);
+
     const {parse, stringify} = require('svgson');
     const svgson = require('svgson');
 
     const [svgData, setSvgData] = useState<mapData>();
 
-    useEffect(() => {
-        // Эмуляция запроса к API для получения SVG-схемы
-        axios.get('http://tsnext.ru/test/frontend/resources/dkl-tsn.svg')
-            .then(async (response) => {
-                // Установка загруженной SVG-схемы в состояние
-                setSvgData(response.data);
+    // useEffect(() => {
+    //     // Эмуляция запроса к API для получения SVG-схемы
+    //     axios.get('http://tsnext.ru/test/frontend/resources/dkl-tsn.svg')
+    //         .then(async (response) => {
+    //             // Установка загруженной SVG-схемы в состояние
+    //             setSvgData(response.data);
+    //
+    //             // Преобразование SVG в объект JSON
+    //             const json = await parse(response.data);
+    //             // const foundObject = json.children.find((item: any) => item.name === "g");
+    //             // console.log(json)
+    //             setSvgData(json)
+    //         })
+    //         .catch(error => {
+    //             console.error('Ошибка загрузки SVG-схемы:', error);
+    //         });
+    // }, []);
 
-                // Преобразование SVG в объект JSON
-                const json = await parse(response.data);
-                // const foundObject = json.children.find((item: any) => item.name === "g");
-                // console.log(json)
-                setSvgData(json)
-            })
-            .catch(error => {
-                console.error('Ошибка загрузки SVG-схемы:', error);
-            });
-    }, []);
+
+        const handleZoomIn = () => {
+        setScale(scale => scale * 1.2);
+    };
+
+    const handleZoomOut = () => {
+        setScale(scale => scale * 0.8);
+    };
+
 
     const {
         className,
@@ -46,12 +61,19 @@ export const MapLoader = memo((props: MapLoaderProps) => {
 
     return (
         <div
-            className={classNames('', mods, [className])}
+            className={classNames(cls.Maploader, mods, [className])}
             {...otherProps}
         >
-            <MapDrag>
-                <Map apiSvg={'http://tsnext.ru/test/frontend/resources/dkl-tsn.svg'}/>
+            <div className={cls.dragWrapper}>
+                            <MapDrag>
+                <Map scale={scale} apiSvg={'https://ci41159.tw1.ru/static/converted.svg'}/>
             </MapDrag>
+
+            </div>
+                <div className={cls.buttonGroup}>
+                    <Button onClick={handleZoomIn}>+</Button>
+                    <Button onClick={handleZoomOut}>-</Button>
+                </div>
 
         </div>
     );
